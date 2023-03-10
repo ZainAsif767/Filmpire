969f362ce3aee42944d4d69c7c2aac818b57ba0f
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-10 19:57:10 +0500

    Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-// import { Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <CssBaseline />
-      App
+      <main>
+        <Routes>
+          <Route exact path="/movies/:id" element={<h1>Movie Information</h1>} />
+          <Route exact path="/actors/:id" element={<h1>Actors</h1>} />
+          <Route exact path="/" element={<h1>Movies</h1>} />
+          <Route exact path="/profile/:id" element={<h1>Profile</h1>} />
+        </Routes>
+      </main>
     </div>
   );
 }

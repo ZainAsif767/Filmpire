8b622e22e4e33cbb1d92b95544ddbe8501208540
Zainasif767 <zainclasher2002@gmail.com>
2023-03-10 16:39:22 +0500

    cmit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div>
+      <CssBaseline />
+      App
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    ,
+);

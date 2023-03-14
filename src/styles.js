873129527b6afd1660554260e2823f76b6091fca
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-14 21:45:52 +0500

    @mui/styles for nav

--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,15 @@
-// import React from 'react';
-// import { stlyle } from 'semantic-ui-react';
+import { makeStyles } from '@mui/material/styles';
 
-// export default style(() => ({
-//   root: {
-//     display: 'flex',
-//     height: '100%',
-//   },
-//   toolbar: {
-//     height: '70px',
-//   },
-//   content: {
-//     flexGrow: '1',
-//     padding: '2em',
-//   },
-// }));
+export default makeStyles(() => ({
+  root: {
+    display: 'flex',
+    height: '100%',
+  },
+  toolbar: {
+    height: '70px',
+  },
+  content: {
+    flexGrow: '1',
+    padding: '2em',
+  },
+}));

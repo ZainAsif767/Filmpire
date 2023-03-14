8e42c9320aaf204ab04bb8ae304dac7d94726bd2
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-14 23:09:46 +0500

    styles for sidebar

---
+++ src/Components/Sidebar/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@mui/material/styles';
+
+export default makeStyles((theme) => ({
+  imageLink: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '10% 0',
+  },
+  image: {
+    width: '70%',
+  },
+  links: {
+    color: theme.palette.text.primary,
+    textDecoration: 'none',
+  },
+  genreImages: {
+    filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)',
+  },
+}));

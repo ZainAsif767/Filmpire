be020b6b3694b4534659f2518a59f50cda5184ed
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-14 21:45:28 +0500

    theemeProvider & createTheme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import App from './App';
+
+const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  ,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 );

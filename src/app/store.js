d76a58982d8651bf8d004ca035101a8e70729cde
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-17 20:02:34 +0500

    store for Provider

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { tmdbApi } from '../services/TMDB';
+
+export default configureStore({
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
+});

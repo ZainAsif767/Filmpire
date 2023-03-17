5cca60d612eaa09de311758c54633bef81ac673d
ZainAsif767 <zainclasher2002@gmail.com>
2023-03-17 20:19:30 +0500

    services for store

---
+++ src/services/TMDB.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+
+// /movie/popular?api_key=API_KEY_HERE&language=en-US&page=1
+
+export const tmdbApi = createApi({
+  reducerPath: 'tmdbApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    //* Get Movies by {Type}
+    getMovies: builder.query({
+      query: () => `/movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+    }),
+  }),
+});
+
+export const { useGetMoviesQuery } = tmdbApi;

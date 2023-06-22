e25031cfd568b3f2cbf3aeb3c50cf5302efb1730
ZainAsif767 <zainclasher2002@gmail.com>
2023-06-22 16:38:10 +0500

    slices for categories

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { tmdbApi } from '../services/TMDB';
+import genreOrCategoryReducer from '../features/currentGenreOrCategry';
 
 export default configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
+    currentGenreOrCategory: genreOrCategoryReducer,
   },
 });

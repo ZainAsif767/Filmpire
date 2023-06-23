cd023861050141d633bc73956ca1d55e148e9546
ZainAsif767 <zainclasher2002@gmail.com>
2023-06-23 18:32:45 +0500

    Fixed

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from "react";
-import { TextField, InputAdornment } from "@mui/material";
-import { Search as SearchIcon } from "@mui/icons-material";
-import { useDispatch, useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { TextField, InputAdornment } from '@mui/material';
+import { Search as SearchIcon } from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
-import { searchMovie } from "../../features/currentGenreOrCategry";
+import { searchMovie } from '../../features/currentGenreOrCategry';
 import useStyles from './styles';
 
 export default function Search() {
-    const [query, setQuery] = useState('');
-    const classes = useStyles();
-    const dispatch = useDispatch();
+  const [query, setQuery] = useState('');
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            dispatch(searchMovie(query));
-        }
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      dispatch(searchMovie(query));
     }
-    return (
-        <div className={classes.searchContainer}>
-            <TextField
-                onKeyPress={handleKeyPress}
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                variant='standard'
-                InputProps={{
-                    className: classes.input,
-                    startAdornment: (
-                        <InputAdornment position='start'>
-                            <SearchIcon />
-                        </InputAdornment>
-                    )
-                }}
-            />
-        </div>
-    );
+  };
+  return (
+    <div className={classes.searchContainer}>
+      <TextField
+        onKeyDown={handleKeyDown}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        variant="standard"
+        InputProps={{
+          className: classes.input,
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </div>
+  );
 }
 

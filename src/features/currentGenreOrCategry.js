f5adff132d0b66b3ae8c81597741520905a3fdcb
ZainAsif767 <zainclasher2002@gmail.com>
2023-06-23 18:37:50 +0500

    fixed

--- src/features/currentGenreOrCategry.js
+++ src/features/currentGenreOrCategry.js
@@ -1,21 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const genreOrCategory = createSlice({
-    name: "genreOrCategory",
-    initialState: {
-        genreIdOrCategoryName: '',
-        page: 1,
-        searchQuery: '',
+  name: 'genreOrCategory',
+  initialState: {
+    genreIdOrCategoryName: '',
+    page: 1,
+    searchQuery: '',
+  },
+  reducers: {
+    selectGenreOrCategory: (state, action) => {
+      state.genreIdOrCategoryName = action.payload;
+      state.searchQuery = '';
     },
-    reducers: {
-        selectGenreOrCategory: (state, action) => {
-            state.genreIdOrCategoryName = action.payload;
-            state.searchQuery = '';
-        },
-        searchMovie: (state, action) => {
-            state.searchQuery = action.payload;
-        },
+    searchMovie: (state, action) => {
+      state.searchQuery = action.payload;
     },
+  },
 });
 
 export const { selectGenreOrCategory, searchMovie } = genreOrCategory.actions;

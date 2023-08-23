a921f2e6a21fbf035b5b3ad31bd7897ecf1d5f42
ZainAsif767 <zainclasher2002@gmail.com>
2023-08-23 20:30:37 +0500

    userLogout

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,16 +1,36 @@
 /* eslint-disable no-console */
 import React from 'react';
+import { Typography, Button, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
-
+import { ExitToApp } from '@mui/icons-material';
 import { userSelector } from '../../features/auth';
 
 export default function Profile() {
   const { user } = useSelector(userSelector);
+  const favoriteMovies = [];
+
   console.log(user);
 
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
   return (
-    <div>
-      Profile - {user.username}
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length
+        ? <Typography variant="h5">Add favorites or watchlist some movies to see them here!</Typography>
+        : (
+          <Box>
+            Favortie Movies
+          </Box>
+        )}
+    </Box>
   );
 }

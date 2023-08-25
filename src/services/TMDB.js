9084585c732e2dc178daf967ea6f2428cfc6ddf2
ZainAsif767 <zainclasher2002@gmail.com>
2023-08-25 20:30:13 +0500

    useGetMovieQuery

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -35,10 +35,16 @@ export const tmdbApi = createApi({
         return `/movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
+
+    //* Get Movie
+    getMovie: builder.query({
+      query: ({ id }) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
 export const {
   useGetGenresQuery,
   useGetMoviesQuery,
+  useGetMovieQuery,
 } = tmdbApi;

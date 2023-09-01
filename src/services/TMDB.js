b36003f7d17c390025cb477a9d27218a6ab61b7b
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-01 23:38:30 +0500

    Actors API

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -46,6 +46,13 @@ export const tmdbApi = createApi({
       // eslint-disable-next-line camelcase
       query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
     }),
+
+    getActorsDetails: builder.query({
+      query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
+    }),
+    getMoviesByActorId: builder.query({
+      query: (id, page) => `/discover/movie?with_cast={id}&page=${page}&api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
@@ -54,4 +61,6 @@ export const {
   useGetMoviesQuery,
   useGetMovieQuery,
   useGetRecommendationsQuery,
+  useGetActorsDetailsQuery,
+  useGetMoviesByActorIdQuery,
 } = tmdbApi;

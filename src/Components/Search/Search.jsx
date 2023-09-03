3506f6da8fa5085a068a1417fe0f58c3b501f044
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-03 08:32:28 +0500

    Feat: find location

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -11,12 +11,16 @@ export default function Search() {
   const [query, setQuery] = useState('');
   const classes = useStyles();
   const dispatch = useDispatch();
+  const location = useLocation();
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
       dispatch(searchMovie(query));
     }
   };
+
+  if (location.pathname !== '/') return null;
+
   return (
     <div className={classes.searchContainer}>
       <TextField

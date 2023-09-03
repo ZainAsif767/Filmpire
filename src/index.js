fb5eebf30adcea2b7a2f6501e4fd31ec63f7a196
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-03 16:43:43 +0500

    Feat: ThemeProvider

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 
+import ToggleColorModeProvider from './utils/ToggleColorMode';
 import App from './App';
 import store from './app/store';
 import './index.css';
 
-const theme = createTheme({});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
+    <ToggleColorModeProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
+    </ToggleColorModeProvider>
   </Provider>,
 );

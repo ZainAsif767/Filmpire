5e67b3916c1d8930bd9388a05028aae00065a568
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-03 08:29:54 +0500

    Pagination route

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 export { default as Actors } from './Actors/Actors.jsx';
 export { default as MovieInformation } from './MovieInformation/MovieInformation.jsx';
 export { default as Movies } from './Movies/Movies.jsx';
@@ -7,3 +8,5 @@ export { default as sidebar } from './Sidebar/Sidebar';
 export { default as MovieList } from './MovieList/MovieList.jsx';
 export { default as Movie } from './Movie/Movie.jsx';
 export { default as Search } from './Search/Search.jsx';
+export { default as Pagination } from './Pagination/Pagination.jsx';
+

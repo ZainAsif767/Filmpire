6107ae680926e14de53fbf36cadd91a8e0ae57cc
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-03 16:45:18 +0500

    Feat: Favorite and watchlisted movies

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable no-console */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography, Button, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { ExitToApp } from '@mui/icons-material';
 import { userSelector } from '../../features/auth';
+import { useGetListQuery } from '../../services/TMDB';
+// eslint-disable-next-line import/no-cycle
+import { RatedCards } from '..';
 
 export default function Profile() {
   const { user } = useSelector(userSelector);
-  const favoriteMovies = [];
 
-  console.log(user);
+  const { data: favoriteMovies, refetch: refechFavorites } = useGetListQuery({ listName: 'favortie/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1 });
+  const { data: watchlistMovies, refetch: refetchWatchlisted } = useGetListQuery({ listName: 'watchlist/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1 });
+
+  useEffect(() => {
+    refechFavorites();
+    refetchWatchlisted();
+  }, []);
 
   const logout = () => {
     localStorage.clear();
@@ -24,11 +32,12 @@ export default function Profile() {
           Logout &nbsp; <ExitToApp />
         </Button>
       </Box>
-      {!favoriteMovies.length
+      {!favoriteMovies?.resuts?.length && !watchlistMovies?.results?.length
         ? <Typography variant="h5">Add favorites or watchlist some movies to see them here!</Typography>
         : (
           <Box>
-            Favortie Movies
+            <RatedCards title="Favorite Movies" data={favoriteMovies} />
+            <RatedCards title="Watchlist" data={watchlistMovies} />
           </Box>
         )}
     </Box>

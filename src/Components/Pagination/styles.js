1cb3c494f6c4e837166f53c455dccf4a9f025d3b
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-03 08:32:03 +0500

    Styles for Paginations

---
+++ src/Components/Pagination/styles.js
@@ -0,0 +1,17 @@
+/* eslint-disable indent */
+import { makeStyles } from '@mui/material/styles';
+
+export default makeStyles((theme) => ({
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        margin: '30px 2px',
+    },
+    pageNumber: {
+        margin: '0 20px !important',
+        color: theme.palette.text.primary,
+    },
+}));

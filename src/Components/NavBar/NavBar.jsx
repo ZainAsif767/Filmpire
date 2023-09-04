345f1bc7cb6f7c0df98736469d7af07842bca5db
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-04 20:22:32 +0500

    Pic fixed

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -88,7 +88,7 @@ export default function NavBar() {
                 <Avatar
                   alt="Profile Picture"
                   style={{ width: 30, height: 30 }}
-                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                  src={`https://www.themoviedb.org/t/w64_and_h64_face${user?.avatar?.tmdb?.avatar_path}`}
                 />
               </Button>
             )}

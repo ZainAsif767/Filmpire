d609de66e966acd39212e69bd6a55c7fdcb961ed
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-04 10:04:22 +0500

    Fix: minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
-import { Actors, Movies, MovieInformation, NavBar, Profile } from './Components';
 
-export default function App() {
+import useStyles from './styles';
+import useAlan from './Components/Alan';
+
+import { Movies, Actors, MovieInformation, NavBar, Profile } from './Components';
+
+function App() {
+  const classes = useStyles();
+  const alanBtnContainer = useRef();
+
+  useAlan();
+
   return (
-    <div className="#">
+    <div className={classes.root}>
       <CssBaseline />
       <NavBar />
-      <main className="#">
-        <div className="#" />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
-          <Route exact path="/movies/:id" element={<MovieInformation />} />
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/approved" element={<Movies />} />
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
           <Route exact path="/actors/:id" element={<Actors />} />
-          <Route exact path={['/', '/approved']} element={<Movies />} />
           <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
       </main>
+      <div ref={alanBtnContainer} />
     </div>
   );
 }
+
+export default App;
